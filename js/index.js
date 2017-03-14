26925c5ef7b771c48e2613a99202986c2e926bc9
alanhovorka <alanhovorka94@gmail.com>
2017-03-14 12:55:45 -0400

    D3 added
    Added D3 to create self-updating numbers for my branding

--- js/index.js
+++ js/index.js
@@ -7,15 +7,39 @@ $(window).scroll(function() {
 				}
 							});
 
-d3.csv("https://docs.google.com/spreadsheets/d/12jMwpmqdbUUfcMHWg2GwGvu-d9BhaJOEsWjK1eoqHRc/pub?output=csv", function(error, data) {
-	data.forEach(function(d) {
-    d.rqt_count = d.rqt_count;
-  	});
-
-		console.log(data[1]);
-		
-		d3.select("#records").append("h3").text(data.rqt_count);
-
-
-	});
-
+d3.csv("https://docs.google.com/spreadsheets/d/12jMwpmqdbUUfcMHWg2GwGvu-d9BhaJOEsWjK1eoqHRc/pub?output=csv", 
+       function(error, data) {
+  d3.select("#requests")
+    .selectAll('h3')    
+      .data(data) 
+      .enter()
+      .append("h3")
+      .text(function(d) {
+        return d.rqt_count;
+      });
+   d3.select("#agencies")
+    .selectAll('h3')    
+      .data(data) 
+      .enter()
+      .append("h3")
+      .text(function(d) {
+        return d.agency_cnt;
+      });
+    d3.select("#records")
+    .selectAll('h3')    
+      .data(data) 
+      .enter()
+      .append("h3")
+      .text(function(d) {
+        return d.records_obt_cnt;
+      });
+    d3.select("#pages")
+    .selectAll('h3')    
+      .data(data) 
+      .enter()
+      .append("h3")
+      .text(function(d) {
+        return d.pg_cnt;
+      })
+      .attr('position', );
+});
