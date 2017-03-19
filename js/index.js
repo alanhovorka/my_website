9531e5fe5a7a596499ee06503b7125bb21c5eec0
alanhovorka <alanhovorka94@gmail.com>
2017-03-19 14:37:18 -0400

    Fix grid system

--- js/index.js
+++ js/index.js
@@ -10,36 +10,46 @@ $(window).scroll(function() {
 d3.csv("https://docs.google.com/spreadsheets/d/12jMwpmqdbUUfcMHWg2GwGvu-d9BhaJOEsWjK1eoqHRc/pub?output=csv", 
        function(error, data) {
   d3.select("#requests")
-    .selectAll('h3')    
+    .selectAll('h1')    
       .data(data) 
-      .enter()
-      .append("h3")
+      .append("h1")
+      .style("color", "#387284")
       .text(function(d) {
         return d.rqt_count;
       });
    d3.select("#agencies")
-    .selectAll('h3')    
+    .selectAll('h1')    
       .data(data) 
-      .enter()
-      .append("h3")
+      .append("h1")
+      .style("color", "#387284")
       .text(function(d) {
         return d.agency_cnt;
       });
     d3.select("#records")
-    .selectAll('h3')    
+    .selectAll('h1')    
       .data(data) 
-      .enter()
-      .append("h3")
+      .append("h1")
+      .style("color", "#387284")
       .text(function(d) {
         return d.records_obt_cnt;
       });
     d3.select("#pages")
-    .selectAll('h3')    
+    .selectAll('h1')    
       .data(data) 
-      .enter()
-      .append("h3")
+      .append("h1")
+      .style("color", "#387284")
       .text(function(d) {
         return d.pg_cnt;
       })
-      .attr('position', );
+      ;
+});
+
+
+// external js: masonry.pkgd.js
+
+var msnry = new Masonry( '.grid', {
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true,
+  stagger: 10
 });
