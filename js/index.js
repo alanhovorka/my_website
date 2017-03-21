0cf9a40c9f21a59dc25e21f6f42e7e9ff84af0fe
alanhovorka <alanhovorka94@gmail.com>
2017-03-21 12:41:28 -0400

    fixed Access Origin error

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/12jMwpmqdbUUf
   }
 
   function showInfo(data, tabletop) {
-    alert('Successfully processed!')
+    console.log('Successfully processed!')
     d3.csv("https://docs.google.com/spreadsheets/d/12jMwpmqdbUUfcMHWg2GwGvu-d9BhaJOEsWjK1eoqHRc/pub?output=csv", 
        function(error, data) {
   d3.select("#requests")
@@ -56,50 +56,9 @@ var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/12jMwpmqdbUUf
 });
   }
 
-  window.addEventListener('DOMContentLoaded', init)
+addEventListener('DOMContentLoaded', init)
 
 
-
-
-
-/*
-d3.csv("https://docs.google.com/spreadsheets/d/12jMwpmqdbUUfcMHWg2GwGvu-d9BhaJOEsWjK1eoqHRc/pub?output=csv", 
-       function(error, data) {
-  d3.select("#requests")
-    .selectAll('h1')    
-      .data(data) 
-      .append("h1")
-      .style("color", "#387284")
-      .text(function(d) {
-        return d.rqt_count;
-      });
-   d3.select("#agencies")
-    .selectAll('h1')    
-      .data(data) 
-      .append("h1")
-      .style("color", "#387284")
-      .text(function(d) {
-        return d.agency_cnt;
-      });
-    d3.select("#records")
-    .selectAll('h1')    
-      .data(data) 
-      .append("h1")
-      .style("color", "#387284")
-      .text(function(d) {
-        return d.records_obt_cnt;
-      });
-    d3.select("#pages")
-    .selectAll('h1')    
-      .data(data) 
-      .append("h1")
-      .style("color", "#387284")
-      .text(function(d) {
-        return d.pg_cnt;
-      })
-      ;
-});
-*/
 
 // external js: masonry.pkgd.js
 
